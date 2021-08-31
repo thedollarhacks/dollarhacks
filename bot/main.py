81e9e5a86fe2718670f8228e643ede4fac460e65
thedollarhacks <89843803+thedollarhacks@users.noreply.github.com>
2021-09-01 00:46:39 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,7 @@ import time
 # from mysql.connector import Error
 # import pandas as pd 
 
-bot = discum.Client(token='NDY5MDUyMTYzMDgzOTkzMDk4.YS5-kA.J6THK54iAljR_CNSiY-NvBqNfzY', log=False)
+bot = discum.Client(token='NDY5MDUyMTYzMDgzOTkzMDk4.YS5_gA.OsHb7dkp_yc6_O9HvAH60nCpAq4', log=False)
 
 bot.sendMessage("882328183461011530", "Damn, I was down. But it feels good to be back! :)")
 
